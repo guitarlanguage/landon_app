19c503eadfaeb33e0f6065691f647a889bbe304b
Tom LaChance <guitarlanguageseries@gmail.com>
2018-06-20 17:16:41 -0500

    add controllers to landon_app. create new php files for the view

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -17,4 +17,24 @@ class ClientController extends Controller
     {
         dd($this->titles);
     }
+
+    public function index() 
+    {
+       return __METHOD__;
+    }
+
+    public function newClient() 
+    {
+        return __METHOD__;
+    }
+
+    public function create() 
+    {
+        return __METHOD__;
+    }
+    
+    public function show($client_id) 
+    {
+        return __METHOD__ . ':' . $client_id;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return '<h3>Landon App Page</h3>';
-    // return view('welcome');
-});
+Route::get('/', 'ContentsController@home');
+Route::get('/clients', 'ClientsController@index');
+Route::get('/clients/new', 'ClientController@newClient');
+Route::post('/clients/new', 'ClientController@create');
+Route::get('/clients/{client_id}', 'ClientController@show');
+Route::post('/clients/{client_id}', 'ClientController@modify');
+
+Route::get('/reservations/{client_id}', 'RoomsController@checkAvailableRooms');
+Route::post('/reservations/{client_id}', 'RoomsController@checkAvailableRooms');
+
+Route::get('/book/room/{client_id}/{room_id}/{date_in}/{date_out}', 'ReservationsController@bookRoom');
+
 
 Route::get('/about', function () {
     $response_arr = [];
@@ -32,3 +40,20 @@ Route::get('/home', function () {
 });
 
 Route::get('/di', 'ClientController@di');
+
+Route::get('/facades/db', function () {
+
+    return DB::select('Select * from table');
+});
+
+Route::get('/facades/encrypt', function () {
+
+    return Crypt::encrypt('123456789');
+});
+
+//eyJpdiI6IllrNXhmTm54RExTWktBaG9NWVN5M3c9PSIsInZhbHVlIjoiK2VNTFc4NzQ3REZtZ2p5blwvTG1rMmdKb2ljZ0tKckdyTmFwTjNhdzRDQlE9IiwibWFjIjoiM2YxOGFmYmI4N2JhZTlkN2QzNDJhMzhmYjJkM2I1M2IwYmU1N2NmZWY4MzRmMDQ4NWJmZWYwNjExYWZhNTFhYiJ9
+
+Route::get('/facades/decrypt', function () {
+
+    return Crypt::decrypt('eyJpdiI6IllrNXhmTm54RExTWktBaG9NWVN5M3c9PSIsInZhbHVlIjoiK2VNTFc4NzQ3REZtZ2p5blwvTG1rMmdKb2ljZ0tKckdyTmFwTjNhdzRDQlE9IiwibWFjIjoiM2YxOGFmYmI4N2JhZTlkN2QzNDJhMzhmYjJkM2I1M2IwYmU1N2NmZWY4MzRmMDQ4NWJmZWYwNjExYWZhNTFhYiJ9');
+});
